0bb3583b8264e55f32f8baddd0cea1f7b683973a
doctor305 <94644276@qq.com>
2017-11-30 11:47:30 +0800

    update

--- maths.py
+++ maths.py
@@ -12,6 +12,7 @@ number = 0
 list_number = []
 list_question = ['','','','']
 list_result = [-1]
+list_error = []
 time_start = 0
 time_end = 0
 number_question = 0
@@ -33,6 +34,7 @@ def get_question():
     global list_result
     global model_list
     global tag_start
+    global tag_done
     canvas.delete('text')
     list_question[0] = random.choice(['+','-'])
     if list_question[0] == '+':
@@ -67,15 +69,16 @@ def get_question():
     list_question[b] = '?'
     string = str(list_question[1])+str(list_question[0])+str(list_question[2])+'='+str(list_question[3])
     canvas.bind('<Button-1>',click)
-    canvas.create_text(100,50,text='第%s题' % n,font='黑体 -40 bold',fill='red',tags='text')    
+    canvas.create_text(100,50,text='第%s题' % number_question,font='黑体 -40 bold',fill='red',tags='text')    
     canvas.create_text(590,250,text=string,font='ComicSansMS -270 bold',fill='blue',tags='text')
     tag_done = 0
 
 def start():
     global time_start
-    global n
-    global number_error 
-    n = 1
+    global number_question
+    global number_error
+    global list_error
+    number_question = 1
     number_error = 0
     time_start = int(time.time())
     get_question()
@@ -85,30 +88,37 @@ def start():
     R2.configure(state = 'disabled')
     R3.configure(state = 'disabled')
     button2.configure(state = 'normal')
+    for s in range(len(list_error)):
+        list_error.pop()
     
 
 def nextquestion():
-    global n
+    global number_question
     global tag_done
     global number_error
-    n += 1
+    global list_error
+    number_question += 1
     if tag_done == 0:
         number_error += 1
+        list_error.append(str(list_question[1])+str(list_question[0])+str(list_question[2])+'='+str(list_question[3]))
     get_question()
     
 def display():
     global time_start
     global time_end
-    global n
+    global number_question
     global number_error
+    global list_error
+    global tag_done
     
     if tag_done == 0:
         number_error += 1
+        list_error.append(str(list_question[1])+str(list_question[0])+str(list_question[2])+'='+str(list_question[3]))
     canvas.delete('text')
     canvas.delete('init')
     time_end = int(time.time())
     canvas.create_text(1180/2,400,text='此次用时：%s 时 %s 分 %s 秒' % ((time_end-time_start)/3600,((time_end-time_start)%3600)/60,((time_end-time_start)%3600)%60),font='ComicSansMS -40 bold',fill='red',tags='text')
-    canvas.create_text(1180/2,500,text='共 %s  错误 %s' % (n,number_error) ,font='ComicSansMS -40 bold',fill='red',tags='text')
+    canvas.create_text(1180/2,500,text='共 %s  错误 %s' % (number_question,number_error) ,font='ComicSansMS -40 bold',fill='red',tags='text')
     button1.configure(text='开始',command=start)
     R1.configure(state = 'normal')
     R2.configure(state = 'normal')
@@ -118,6 +128,7 @@ def display():
 def click(event):
     global number_error
     global tag_done
+    global list_error
     x = event.x
     y = event.y
     for xy in list_number:
@@ -130,6 +141,7 @@ def click(event):
             else:
                 canvas.create_text(700,400,text='错误，正确答案应为 '+str(list_result[0]),font='ComicSansMS -40 bold',fill='red',tags='text')
                 number_error += 1
+                list_error.append(str(list_question[1])+str(list_question[0])+str(list_question[2])+'='+str(list_question[3]))
                 #print number_error
             break
             ##print xy.number,list_result[0]
